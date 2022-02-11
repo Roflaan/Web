38a56dde7c44a492c95d29e5ffb1d678f08a04b5
Sergey <sergey.konev.ru@gmail.com>
2022-02-11 09:47:24 +0200

    2.2

--- Lab2/code/index.php
+++ Lab2/code/index.php
@@ -6,3 +6,13 @@ $order = &$very_bad_unclear_name;
 $order .= " and large fries";
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+//2
+$varInt = 25;
+echo "<br/>$varInt<br/>";
+$varFloat = 5.25;
+echo "$varFloat<br/>";
+echo $varInt - 13, "<br/>";
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo "Difference between expences: ", $last_month - $this_month, "<br/>";
