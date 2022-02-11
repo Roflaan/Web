a3db48c439b043d80d4f19ca0720bdf0e074048e
Sergey <sergey.konev.ru@gmail.com>
2022-02-11 21:14:58 +0200

    2.20

--- Lab2/code/index.php
+++ Lab2/code/index.php
@@ -310,3 +310,22 @@ for ($i=1; $i<=20; $i++){
     }
     echo "<br/>";
 }
+
+//20
+$arr = array(2,5,3,9);
+echo array_sum($arr)/sizeof($arr), "<br/>";
+echo array_sum(range(1, 100)), "<br/>";
+
+$arr = array_map('sqrt', $arr);
+print_r($arr);
+echo "<br/>";
+
+$keys = range("a", "z");
+$nums = range(1, 26);
+$arr = array_combine($keys, $nums);
+print_r($arr);
+echo "<br>";
+
+$str = "1234567890";
+$arr = str_split($str, 2);
+echo array_sum($arr);
