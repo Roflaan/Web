5458acb7c2dec7061f781eb39926a9b2c4515d78
Sergey <sergey.konev.ru@gmail.com>
2022-02-21 14:56:40 +0200

    Lab 3.1

---
+++ Lab/code/lab3.php
@@ -0,0 +1,16 @@
+<?php
+
+// Task1
+
+//a
+preg_match_all("/a.{2}b/", "ahb acb aeb aeeb adcb axeb", $array);
+print_r($array);
+echo "<br>";
+
+//b
+echo preg_replace_callback(
+    "/[0-9]+/",
+    function ($matches){
+        return $matches[0] ** 2;
+    },
+    "a1b2c3");
